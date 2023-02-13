1fe5c0cb37091a534fcf9beb04be92f8269a2f43
Stephane <pub.sg@free.fr>
2023-02-13 15:36:42 +0100

    quatrieme

--- demo.py
+++ demo.py
@@ -5,10 +5,16 @@ def premiere_fonction():
 def deuxieme_fonction ():
     print ("deuxieme fonction")
 
+def quatrieme ():
+    print ("quatrieme")
+
+
 def troisieme_fonction ():
     premiere_fonction ()
     deuxieme_fonction ()
 
 print ("bonjour")
 troisieme_fonction ()
+quatrieme ()
+
 
